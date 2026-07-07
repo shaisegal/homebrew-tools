772d04a17a38aab31949671798760fab8a5afd01
shaisegal <karl.rannseier@gmail.com>
2026-07-08 00:17:39 +0200

    Add apple-photos-to-immich formula

---
+++ Formula/apple-photos-to-immich.rb
@@ -0,0 +1,45 @@
+class ApplePhotosToImmich < Formula
+  include Language::Python::Virtualenv
+
+  desc "Migrate Apple Photos libraries to Immich"
+  homepage "https://github.com/shaisegal/apple-photos-to-immich"
+  url "https://github.com/shaisegal/apple-photos-to-immich/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "9b430902257048269494baa62ed2de6927d668cbd3f3e0e4adbbbc858c018312"
+  license "MIT"
+
+  depends_on "python@3.12"
+
+  resource "requests" do
+    url "https://files.pythonhosted.org/packages/ac/c3/e2a2b89f2d3e2179abd6d00ebd70bff6273f37fb3e0cc209f48b39d00cbf/requests-2.34.2.tar.gz"
+    sha256 "f288924cae4e29463698d6d60bc6a4da69c89185ad1e0bcc4104f584e960b9ed"
+  end
+
+  resource "certifi" do
+    url "https://files.pythonhosted.org/packages/c9/c7/424b75da314c1045981bd9777432fad05a9e0c69daa4ed7e308bbaffe405/certifi-2026.6.17.tar.gz"
+    sha256 "024c88eeec92ca068db80f02b8b07c9cef7b9fe261d1d535abfd5abd6f6af432"
+  end
+
+  resource "charset-normalizer" do
+    url "https://files.pythonhosted.org/packages/bd/2a/23f34ec9d04624958e137efdc394888716353190e75f25dd22c7a2c7a8aa/charset_normalizer-3.4.9.tar.gz"
+    sha256 "673611bbd43f0810bec0b0f028ddeaaa501190339cac411f347ac76917c3ae7b"
+  end
+
+  resource "idna" do
+    url "https://files.pythonhosted.org/packages/cd/63/9496c57188a2ee585e0f1db071d75089a11e98aa86eb99d9d7618fc1edce/idna-3.18.tar.gz"
+    sha256 "ffb385a7e039654cef1ab9ef32c6fafe283c0c0467bba1d9029738ce4a14a848"
+  end
+
+  resource "urllib3" do
+    url "https://files.pythonhosted.org/packages/53/0c/06f8b233b8fd13b9e5ee11424ef85419ba0d8ba0b3138bf360be2ff56953/urllib3-2.7.0.tar.gz"
+    sha256 "231e0ec3b63ceb14667c67be60f2f2c40a518cb38b03af60abc813da26505f4c"
+  end
+
+  def install
+    virtualenv_install_with_resources
+    bin.install_symlink libexec/"bin/apple-photos-to-immich"
+  end
+
+  test do
+    assert_match "apple-photos-to-immich", shell_output("#{bin}/apple-photos-to-immich --help")
+  end
+end
